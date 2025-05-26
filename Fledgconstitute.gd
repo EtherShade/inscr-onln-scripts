e1546c56033f8f5e05f1dc2a9339bbc5a6730102
EtherShade <135162361+EtherShade@users.noreply.github.com>
2025-05-26 17:46:51 -0400

    Update Fledgconstitute.gd

--- Fledgconstitute.gd
+++ Fledgconstitute.gd
@@ -7,20 +7,19 @@ func start_of_turn(cardAnim):
 	var dmgTaken = card.card_data["health"] - card.health
 	card.from_data(CardInfo.from_name(card.card_data["evolution"]))
 	card.health = card.card_data["health"] - dmgTaken
-	print("Reconstitute triggered!")
-		
-		var old_data = card.card_data.duplicate()
 
-		if card.card_data.name == "Ouroboros":
-			old_data.attack += 1
-			old_data.health += 1
+	print("Reconstitute triggered!")	
+	var old_data = card.card_data.duplicate()
+	if card.card_data.name == "Ouroboros":
+		old_data.attack += 1
+		old_data.health += 1
 
-		fightManager.gold_sarcophagus.append(
-			{
-				"card": old_data,
-				"turnsleft": 1
-			}
-		)
+	fightManager.gold_sarcophagus.append(
+		{
+			"card": old_data,
+			"turnsleft": 1
+		}
+	)
 	# Calculate buffs
 	slotManager.recalculate_buffs_and_such()
 #	for card in slotManager.all_friendly_cards():
